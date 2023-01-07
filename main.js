76c4f4a40e50544ced4c973b0c1a131327cb999c
anahitanail9 <93966633+anahitanail9@users.noreply.github.com>
2023-01-07 10:16:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+Webcam.set({
+  width:350,
+  height:300,
+  image_format  : 'png',
+  png_quality:90
+  });
+  camera= document.getElementById("camera");
+  Webcam.attach('#camera');
+  function take_snapshot()
+  {
+   Webcam.snap(function(data_uri){
+      document.getElementById("result").innerHTML= '<img id="captured_image" src="'+data_uri+'"/>';
+   });
+  }
+  console.log('ml5 version:', ml5.version);
+  classifier = ml5.imageClassifier('https://unpkg.com/ml5@0.4.3/dist/ml5.min.js ',modelLoaded)
+  function modelLoaded(){
+      console.log('Model Loaded!');
+  }
+  function speak(){
+      var synth = window.speechSynthesis;
+      speak_data_1 = "The first prediction is"+ prediction_1;
+      speak_data_2 = "and the second prediction is"+ prediction_2;
+      var_utterThis = new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+      synth.speak(utterThis);
+  }
+  
